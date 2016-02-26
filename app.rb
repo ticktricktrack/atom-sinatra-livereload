89450ff135ce6749b46183f9f690841aa565d6ea
Rainer Kuhn <ticktricktrack@gmail.com>
2016-02-26 10:31:50 +0000

    use guard-livereload

--- app.rb
+++ app.rb
@@ -1,19 +1,20 @@
 require "sinatra/base"
 require "haml"
 require "sass"
-require 'sinatra/assetpack'
+require "sinatra/assetpack"
 
 class App < Sinatra::Base
-    set :root, File.dirname(__FILE__) # You must set app root
-    register Sinatra::AssetPack
+  set :root, File.dirname(__FILE__)
+  set :views, Proc.new { File.join(root, "app", "views") }
+  register Sinatra::AssetPack
 
-    assets {
-      serve '/js',     from: 'app/js'        # Default
-      serve '/css',    from: 'app/css'       # Default
-      serve '/images', from: 'app/images'    # Default
-    }
+  assets {
+    serve "/js",     from: "app/js"        # Default
+    serve "/css",    from: "app/css"       # Default
+    serve "/images", from: "app/images"    # Default
+  }
 
-    get '/' do
-      haml :dashboard
-    end
+  get "/" do
+    haml :dashboard
+  end
 end
